fb7eda68068f4cb57a19f0e7f8dd5ce654411d80
sobocanv <161503794+sobocanv@users.noreply.github.com>
2025-05-03 11:14:33 +0200

    Update explicits.js

--- explicits.js
+++ explicits.js
@@ -7,11 +7,11 @@ define(['questAPI'], function(Quest){
 	*/
     API.addPagesSet('basicPage',{
         noSubmit:false, //Change to true if you don't want to show the submit button.
-        header: 'Questionnaire',
+        header: 'Vprašalnik',
         decline: true,
-        declineText: isTouch ? 'Decline' : 'Decline to Answer', 
+        declineText: isTouch ? 'Ne' : 'Nočem odgovoriti', 
         autoFocus:true, 
-        progressBar:  'Page <%= pagesMeta.number %> out of 3'
+        progressBar:  'Stran <%= pagesMeta.number %> od 3'
     });
 	
     /**
@@ -22,13 +22,13 @@ define(['questAPI'], function(Quest){
         required : true, 		
         errorMsg: {
             required: isTouch 
-                ? 'Please select an answer, or click \'Decline\'' 
-                : 'Please select an answer, or click \'Decline to Answer\''
+                ? 'Prosimo, izberite ustrezni odgovor ali pritisnite na gumb \'Ne\'' 
+                : 'Prosimo, izberite ustrezni odgovor ali pritisnite na gumb \'Nočem odgovoriti\''
         },
         autoSubmit:'true',
         numericValues:'true',
         help: '<%= pagesMeta.number < 3 %>',
-        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
+        helpText: 'Nasvet: Če želite odgovoriti hitreje, pritisnite na izbrani odziv dvakrat.'
     });
 
     API.addQuestionsSet('basicSelect',{
@@ -45,17 +45,17 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('therm',{
         inherit: 'basicSelect',
         answers: [
-            {text:'10 - Extremely warm', value:10},
-            {text:'9 - Very warm', value:9},
-            {text:'8 - Moderately warm', value:8},
-            {text:'7 - Somewhat warm', value:7},
-            {text:'6 - Slightly warm', value:6},
-            {text:'5 - Neither warm nor cold', value:5},
-            {text:'4 - Slightly cold', value:4},
-            {text:'3 - Somewhat cold', value:3},
-            {text:'2 - Moderately cold', value:2},
-            {text:'1 - Very cold', value:1},
-            {text:'0 - Extremely cold', value:0}
+            {text:'10 - Izjemno toplo', value:10},
+            {text:'9 - Zelo toplo', value:9},
+            {text:'8 - Zmerno toplo', value:8},
+            {text:'7 - Nekoliko toplo', value:7},
+            {text:'6 - Rahlo toplo', value:6},
+            {text:'5 - Niti toplo niti hladno', value:5},
+            {text:'4 - Rahlo hladno', value:4},
+            {text:'3 - Nekoliko hladno', value:3},
+            {text:'2 - Zmerno hladno', value:2},
+            {text:'1 - Zelo hladno', value:1},
+            {text:'0 - Izjemno hladno', value:0}
         ]
     });
 
@@ -66,28 +66,28 @@ define(['questAPI'], function(Quest){
     API.addQuestionsSet('attributes7',{
         inherit : 'basicSelect',
         name: 'attributes7',
-        stem: 'Which statement best describes you?',
+        stem: 'Katera izjava vas najbolje opisuje?',
         answers: [
-            {text:'I strongly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:7},
-            {text:'I moderately prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:6},
-            {text:'I slightly prefer <%= global.whiteLabels %> to <%= global.blackLabels %>.',value:5},
-            {text:'I like <%= global.whiteLabels %> and <%= global.blackLabels %> equally.',value:4},
-            {text:'I slightly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:3},
-            {text:'I moderately prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:2},
-            {text:'I strongly prefer <%= global.blackLabels %> to <%= global.whiteLabels %>.',value:1}
+            {text:'Močno raje imam <%= global.whiteLabels %> kot <%= global.blackLabels %>.',value:7},
+            {text:'Zmerno raje imam <%= global.whiteLabels %> kot <%= global.blackLabels %>.',value:6},
+            {text:'Nekoliko raje imam <%= global.whiteLabels %> kot <%= global.blackLabels %>.',value:5},
+            {text:'Enako rad imam <%= global.whiteLabels %> in <%= global.blackLabels %>.',value:4},
+            {text:'Nekoliko raje imam <%= global.blackLabels %> kot <%= global.whiteLabels %>.',value:3},
+            {text:'Zmerno raje imam <%= global.blackLabels %> kot <%= global.whiteLabels %>.',value:2},
+            {text:'Močno raje imam <%= global.blackLabels %> kot <%= global.whiteLabels %>.',value:1}
         ]
     });
 	
     API.addQuestionsSet('thermBlack',{
         inherit : 'therm',
         name: 'Tblack_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.blackLabels %></b>?'
+        stem: 'Kako toplo ali hladno se počutite do <b><%= global.blackLabels %></b>?'
     });
 
     API.addQuestionsSet('thermWhite',{
         inherit : 'therm',
         name: 'Twhite_0to10',
-        stem: 'How warm or cold do you feel towards <b><%= global.whiteLabels %></b>?'
+        stem: 'Kako toplo ali hladno se počutite do <b><%= global.whiteLabels %></b>?'
     });
 
     API.addSequence([
