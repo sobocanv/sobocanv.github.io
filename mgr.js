5fac49e662f2d9c57a51e39c03a2f4e3eefc6a39
sobocanv <161503794+sobocanv@users.noreply.github.com>
2025-05-03 10:27:43 +0200

    Update mgr.js

--- mgr.js
+++ mgr.js
@@ -9,7 +9,7 @@ define(['managerAPI',
 
 	var API    = new Manager();
 	//const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-	init_data_pipe(API, 'zpjKaPesdEOI',  {file_type:'csv'});	
+	init_data_pipe(API, 'N1NLIFf6LdYh',  {file_type:'csv'});	
 
     API.setName('mgr');
     API.addSettings('skip',true);
