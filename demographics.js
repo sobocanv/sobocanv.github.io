7e3d64d111650b67bf849ac0bf0bf7ce69ac3e52
sobocanv <161503794+sobocanv@users.noreply.github.com>
2025-05-04 14:57:32 +0200

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -11,7 +11,7 @@ define(['questAPI'], function(Quest){
         decline: true,
         declineText: isTouch ? 'Decline' : 'Decline to Answer', 
         autoFocus:true, 
-        progressBar: 'Page <%= pagesMeta.number %> out of <%= pagesMeta.count %>'
+        progressBar: 'Page <%= pagesMeta.number %> out of 5'
     });
 
     /**
