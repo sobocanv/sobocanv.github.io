990f19423c1b1392aa34e6bf8dfed73a5465fe15
sobocanv <161503794+sobocanv@users.noreply.github.com>
2025-05-07 09:45:09 +0200

    Update demographics.js

--- demographics.js
+++ demographics.js
@@ -42,7 +42,7 @@ define(['questAPI'], function(Quest){
 	    name:'birth_Month',
 	    stem:'V katerem mesecu ste rojeni?',
 	    style:'multiButtons',
-	    minWidth:'10%',
+	    minWidth:'8%',
 	    answers: [
             	'Januar', 'Februar', 'Marec', 'April',
 		'Maj', 'Junij', 'Julij', 'Avgust',
